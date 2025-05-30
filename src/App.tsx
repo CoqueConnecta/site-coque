7c2f2a3b237026462b151a788f7b0edcb2d66ce1
Gildo Neto <32935771+gildoneto@users.noreply.github.com>
2025-05-29 21:52:36 -0300

    refactor App component to use Coque logo and update content

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import coqueLogo from './assets/coque-logo-vertical.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="http://www.coqueconnecta.ong.br" target="_blank">
+          <img src={coqueLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Site Coque Connecta</h1>
       <h2>Em construção!</h2>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Em breve você poderá acessar o site da Coque Connecta
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
