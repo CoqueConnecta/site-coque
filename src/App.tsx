30ac4bd139c8cd476af827a26b1cc2ca92a984d6
Gildo Neto <32935771+gildoneto@users.noreply.github.com>
2025-05-29 21:44:33 -0300

    update title and heading to reflect project name and status

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,8 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Site Coque Connecta</h1>
+      <h2>Em construção!</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
