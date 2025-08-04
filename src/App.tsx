abfd460504a43c14d687593480cf57419a8b4045
Gildo Neto <gildoneto@hotmail.com>
2025-08-04 12:02:59 -0300

    Add MissionVisionValues component and update translations for mission, vision, and values

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Banner from './components/Banner/Banner'
 import Contacts from './components/Contacts/Contacts'
 import Header from './components/Header/Header'
 import LanguageToggle from './components/LanguageToggle/LanguageToggle'
+import MissionVisionValues from './components/MissionVisionValues/MissionVisionValues'
 import Projects from './components/Projects/Projects'
 import WantHelp from './components/WantHelp/WantHelp'
 import WhoWeAre from './components/WhoWeAre/WhoWeAre'
@@ -13,7 +14,7 @@ function App() {
       <LanguageToggle/>
       <Header/>
       <Banner/>
-      <WhoWeAre/>
+      <MissionVisionValues/>
       <Projects/>
       <WantHelp/>
       <Contacts/>
